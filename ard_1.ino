a3f40cd7c20f6c2b4cb8ee1fc7963a28045f7cc4
martai4 <80327139+martai4@users.noreply.github.com>
2021-03-29 14:03:44 +0200

    Update ard_1.ino

--- ard_1.ino
+++ ard_1.ino
@@ -1,10 +1,13 @@
- // constants won't change. Used here to 
+// constants won't change. Used here to 
  // set pin numbers:
 const int stepPin = 2;      // the number of the step pin
 const int dirPin = 3;         // the number of the direction pin
 const int stepPin2 = 8;
 const int dirPin2 = 9;
 const int ledPin = 13;      // the number of the LED pin
+const int czujnikPin = 0;
+const int liczba_pomiarow = 30;
+const int maks_obrot=50;
 
 // Variables will change:
 int ledState = LOW;             // ledState used to set the LED
@@ -15,10 +18,16 @@ long previousMillis = 0;        // will store last time LED was updated
 // will quickly become a bigger number than can be stored in an int.
 long interval = 200;           // interval at which to blink (milliseconds)
 int timesOneDirection = 0;
+int tryb_pracy;
+int ile_lewo = 0;
+int ile_gora = 0;
+int najlepsza_pozycja_lewo = 0;
+int najlepsza_pozycja_gora = 0;
+int najlepsza_wartosc = 0;
+int ruch = 30;
 
 void setup() {
     // set the digital pin as output:
-    Serial.begin(9600);
     pinMode(ledPin, OUTPUT);
     pinMode(stepPin, OUTPUT);
     pinMode(dirPin, OUTPUT);
@@ -26,7 +35,7 @@ void setup() {
     pinMode(dirPin2, OUTPUT);
     digitalWrite(stepPin2, LOW);
     digitalWrite(dirPin2, LOW);
-    wyswietl_panel_startowy();
+    wyswietl_panel_startowy1();
 }
 
 void loop()
@@ -37,41 +46,78 @@ void loop()
     // difference between the current time and last time you blinked 
     // the LED is bigger than the interval at which you want to 
     // blink the LED.
-    
+
     String odczytano;
     
-    if(Serial.available()>0){
-          odczytano = Serial.readStringUntil('\n');
-          int odczyt=odczytano[0];
-          switch (odczyt) {
-          case 68://D
-              prawo_o_krok();
+    if (tryb_pracy == 2) {//manualny
+        if (Serial.available() > 0) {
+            odczytano = Serial.readStringUntil('\n');
+            int odczyt = odczytano[0];
+            switch (odczyt) {
+            case 68://D
+                prawo_o_krok();
+                ile_lewo=(ile_lewo-1)%200;
+                break;
+            case 65://A
+                lewo_o_krok();
+                ile_lewo = (ile_lewo + 1) % 200;
+                break;
+            case 87://W
+                gora_o_krok();
+                ile_gora = (ile_gora + 1) % 200;
+                break;
+            case 83://S
+                dol_o_krok();
+                ile_gora = (ile_gora - 1) % 200;
+                break;
+            case 80://P
+                pomiar();
+                break;
+            case 66://B
+                ustaw_na_najlepsza();
+                break;
+            case 77://M
+              ustaw_na_pocztek();
+            break;
+            case 88://X
+                ustaw_na_pocztek();
+                delay(1000);
+                exit(0);
+                break;
+            default:
+                Serial.println("Zly wybor, wybierz jeszcze raz");
+            }
+        }
+    }
+    else {//automatyczny
+
+        if (Serial.available() > 0) {
+            odczytano = Serial.readStringUntil('\n');
+            int odczyt = odczytano[0];
+            switch(odczyt){
+              case 83://S-start
+              przesuwaj();
+              ustaw_na_najlepsza();
               break;
-          case 65://A
-              lewo_o_krok();
+              case 67://C-kontynuuj precyzje
+              ruch=ruch/2;
               break;
-          case 87://W
-              gora_o_krok();
+              case 77://M
+              ustaw_na_pocztek();
               break;
-          case 83://S
-              dol_o_krok();
-              break;
-          case 80://P
-              pomiar();
-              break;
-          case 66://B
-              Serial.println("Opcja w wykonaniu");
-              //ustaw_na_najlepsza();
-              break;
-          case 88://X
-              delay(1000);
+              case 88://X
+              ustaw_na_pocztek();
+              delay(10);
               exit(0);
               break;
-          default:
+              default:
               Serial.println("Zly wybor, wybierz jeszcze raz");
-          }
+            }
+            
+        }
+        
     }
-  //swiecenie diody
+    //swiecenie diody
     unsigned long currentMillis = millis();
     if (currentMillis - previousMillis > interval) {
         // save the last time you blinked the LED 
@@ -86,17 +132,157 @@ void loop()
         digitalWrite(ledPin, ledState);
     }
 }
+
+
+void przesuwaj() {
+    //-------------------------------------------
+    for (int i = 0; i < ruch; i++) {
+        if(ile_lewo<maks_obrot){
+           lewo_o_krok();
+           ile_lewo=(ile_lewo+1)%200;
+        }
+    }//0L->1L 0G
+    for (int i = 0; i < ruch; i++) {
+        if(ile_gora<maks_obrot){
+           gora_o_krok();
+           ile_gora=(ile_gora+1)%200;
+        }
+    }//1L 0G->1G
+    delay(1000);
+    pomiar();//1L 1G
+    for (int i = 0; i < ruch; i++) {
+        if(ile_gora>-maks_obrot){
+           dol_o_krok();
+           ile_gora=(ile_gora-1)%200;
+        }
+    }//1L 1G->0G
+    delay(1000);
+    pomiar();//1L 0G
+    for (int i = 0; i < ruch; i++) {
+        if(ile_gora>-maks_obrot){
+           dol_o_krok();
+           ile_gora=(ile_gora-1)%200;
+        }
+    }//1L 0G->-1G
+    delay(1000);
+    pomiar();//1L -1G
+    //-------------------------------------------
+    for (int i = 0; i < ruch; i++) {
+        if(ile_lewo>-maks_obrot){
+           prawo_o_krok();
+           ile_lewo=(ile_lewo-1)%200;
+        }
+    }//1L->0L -1G
+    delay(1000);
+    pomiar();//0L -1G
+    for (int i = 0; i < ruch; i++) {
+        if(ile_gora<maks_obrot){
+           gora_o_krok();
+           ile_gora=(ile_gora+1)%200;
+        }
+    }//0L -1G->0G
+    delay(1000);
+    pomiar();//0L 0G
+    for (int i = 0; i < ruch; i++) {
+        if(ile_gora<maks_obrot){
+           gora_o_krok();
+           ile_gora=(ile_gora+1)%200;
+        }
+    }//0L 0G->1G
+    delay(1000);
+    pomiar();//0L 1G
+    //-------------------------------------------
+    for (int i = 0; i < ruch; i++) {
+        if(ile_lewo>-maks_obrot){
+           prawo_o_krok();
+           ile_lewo=(ile_lewo-1)%200;
+        }
+    }//0L->-1L 1G
+    delay(1000);
+    pomiar();//-1L 1G
+    for (int i = 0; i < ruch; i++) {
+        if(ile_gora>-maks_obrot){
+           dol_o_krok();
+           ile_gora=(ile_gora-1)%200;
+        }
+    }//-1L 1G->0G
+    delay(1000);
+    pomiar();//-1L 0G
+    for (int i = 0; i < ruch; i++) {
+        if(ile_gora>-maks_obrot){
+           dol_o_krok();
+           ile_gora=(ile_gora-1)%200;
+        }
+    }//-1L 0G->-1G
+    delay(1000);
+    pomiar();//-1L -1G
+    //-------------------------------------------
+}
+void ustaw_na_najlepsza() {
+    while (najlepsza_pozycja_lewo > ile_lewo) {
+        lewo_o_krok();
+        ile_lewo=(ile_lewo+1)%200;
+    }
+    Serial.println("Ustawiono lewo");
+    while (najlepsza_pozycja_lewo < ile_lewo) {
+        prawo_o_krok();
+        ile_lewo=(ile_lewo-1)%200;
+    }
+    Serial.println("Ustawiono prawo");
+    while (najlepsza_pozycja_gora > ile_gora) {
+        gora_o_krok();
+        ile_gora=(ile_gora+1)%200;
+    }
+    Serial.println("Ustawiono gora");
+    while (najlepsza_pozycja_gora < ile_gora) {
+        dol_o_krok();
+        ile_gora=(ile_gora-1)%200;
+    }
+    Serial.println("Ustawiono dol");
+
+}
+
+void ustaw_na_pocztek() {
+    while (ile_lewo < 0) {
+        lewo_o_krok();
+        ile_lewo=(ile_lewo+1)%200;
+    }
+    Serial.println("Ustawiono lewo");
+    while (ile_lewo > 0) {
+        prawo_o_krok();
+        ile_lewo=(ile_lewo-1)%200;
+    }
+    Serial.println("Ustawiono prawo");
+    while (ile_gora< 0) {
+        gora_o_krok();
+        ile_gora=(ile_gora+1)%200;
+    }
+    Serial.println("Ustawiono gora");
+    while (ile_gora > 0) {
+        dol_o_krok();
+        ile_gora=(ile_gora-1)%200;
+    }
+    Serial.println("Ustawiono dol");
+
+}
 void pomiar() {
-    int wartosc_z_czujnika;
-    //gdzie jest podpiety czujnik (ADC)
-    wartosc_z_czujnika = analogRead(7);
+    int wartosc_z_czujnika = 0;
+    for (int a = 0; a < liczba_pomiarow; a++) {
+        wartosc_z_czujnika += analogRead(0);
+    }
+    wartosc_z_czujnika = wartosc_z_czujnika / liczba_pomiarow;
     Serial.println(wartosc_z_czujnika);
     //powinny byc wartosci 0-1023
+    if (najlepsza_wartosc < wartosc_z_czujnika) {
+        najlepsza_wartosc = wartosc_z_czujnika;
+        najlepsza_pozycja_lewo = ile_lewo;
+        najlepsza_pozycja_gora = ile_gora;
+    }
     delay(100);
 }
 
-void wyswietl_panel_startowy() {
-    Serial.println("Zaczynamy zabawe!");
+
+void wyswietl_panel_startowy2() {
     Serial.println("Co chcesz zrobic?");
     Serial.println("1. [A] Przesun w lewo o krok");
     Serial.println("2. [D] Przesun w prawo o krok");
@@ -104,8 +290,47 @@ void wyswietl_panel_startowy() {
     Serial.println("4. [S] Przesun w dol o krok");
     Serial.println("5. [P] Dokonaj pomiaru");
     Serial.println("6. [B] Ustaw na najlepsza pozycje (z tych zmierzonych)");
-    Serial.println("7. [X] Zakoncz");
+    Serial.println("7. [M] Ustaw na pozycje poczatkowa");
+    Serial.println("8. [X] Zakoncz");
     Serial.println("Podaj znak wybranej opcji: ");
+}
+
+void wyswietl_panel_startowy3() {
+    Serial.println("Co chcesz zrobic?");
+    Serial.println("1. [S] Sprawdzaj automatycznie");
+    Serial.println("2. [C] Zwieksz precyzje");
+    Serial.println("3. [M] Ustaw na pozycje poczatkowa");
+    Serial.println("4. [X] Zakoncz");
+    Serial.println("Podaj znak wybranej opcji: ");
+}
+void wyswietl_panel_startowy1() {
+    Serial.begin(9600);
+    Serial.println("Zaczynamy zabawe!");
+    Serial.println("Wybierz tryb dzialania");
+    Serial.println("1. Tryb automatyczny");
+    Serial.println("2. Tryb manualny");
+    Serial.println("Podaj numer wybranej opcji: ");
+
+    String odczytano1;
+    int odczyt1=0;
+    while (odczyt1!=2 && odczyt1!=1) {
+          odczytano1 = Serial.readStringUntil('\n');
+          int odczyt1 = odczytano1[0]-48;
+          if(odczyt1==2){
+            Serial.println("Wybrano tryb manualny");
+            tryb_pracy=2;
+            wyswietl_panel_startowy2();
+            break;
+          }
+          if(odczyt1==1){
+            Serial.println("Wybrano tryb automatyczny");
+            tryb_pracy = 1;
+            wyswietl_panel_startowy3();
+            break;
+          }
+          delay(100);
+      }
+    
 }
 
 void lewo_o_krok() {
